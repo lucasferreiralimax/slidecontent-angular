b22280a0fd53ed6cc45f6bebb95ae46a8c821506
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-02-05 01:08:41 -0300

    adjustment title app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'slidecontent-angular';
+  title = 'SlideContentAngular';
 }
