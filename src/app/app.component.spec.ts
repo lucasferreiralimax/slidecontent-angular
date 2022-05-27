e2e0a573e2ef7f5a5f2f3f62c374d59f2249e6b8
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-05-27 02:30:26 -0300

    adjustment test karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('slidecontent-angular app is running!');
+    expect(compiled.querySelector('.app > h1')?.textContent).toContain('slidecontent-angular');
   });
 });
