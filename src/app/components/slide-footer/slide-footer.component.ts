0f21c7dc98117bac5d6cbe6b1e9b62a5c3a19d6a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-09-29 15:00:09 -0300

    refactory footer with input binding

--- src/app/components/slide-footer/slide-footer.component.ts
+++ src/app/components/slide-footer/slide-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-slide-footer',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slide-footer.component.scss']
 })
 export class SlideFooterComponent implements OnInit {
+
+  @Input()  title: string;
 
   constructor() { }
 
